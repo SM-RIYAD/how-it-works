fc818c91800bf548367f45b532c3ebd3f5442a16
SM RIYAD <smriyad0185@gmail.com>
2024-01-25 13:02:17 +0600

    added readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 How it works project
 
+To run this project locally:
+
+with npm,
+run following command:
+
+npm install
+npm run dev
+
+OR,
+
+with yarn,
+run following command:
+yarn
+yarn dev
+
+I used tailwind css,react in this project.
--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import "./App.css";
-import Circle from "./components/Circle";
 
 function App() {
   return (
     <div className=" w-full p-20 mb-20">
-      <h1 className="text-4xl font-bold text-center ms-10  lg:ms-0 text-black lg:my-20">
-        How it works
-      </h1>
+      {/* this is the header  */}
+      <div className="w-full">
+        <h1 className="text-4xl font-bold text-center ms-10  lg:ms-0 text-black lg:my-20">
+          How it works
+        </h1>
+      </div>
 
       <div className="relative mt-56 ">
+        {/* this is first circle */}
         <div className="bg-blue-500 flex items-center justify-center lg:h-[160px]  lg:w-[160px] h-[200px] w-[200px] rounded-full">
           <div>
-            <img className="ps-5 hover:scale-150 transition ease-in-out delay-100 hover:rotate-180" src="/images/goal 2.png" alt="" srcset="" />
+            <img
+              className="ps-5 hover:scale-150 transition ease-in-out delay-100 hover:rotate-180"
+              src="https://i.ibb.co/tK0wgG0/goal-2.png"
+              alt=""
+            />
             <p className="text-white ps-5  font-bold text-base"> Step 1: </p>
 
             <p className="text-white   font-bold text-base"> Set your goal</p>
@@ -20,21 +27,22 @@ function App() {
 
         <img
           className="absolute lg:bottom-[150px]  lg:left-[130px] bottom-[200px]  left-[130px]"
-          src="/images/Arrow 1.png"
+          src="https://i.ibb.co/x23zzHX/Arrow-1.png"
           alt=""
         />
+        {/* this is second circle */}
         <div className="bg-blue-500 flex items-center justify-center lg:h-[160px] lg:w-[160px] h-[200px] w-[200px] rounded-full absolute left-[325px] bottom-[190px]">
           <div>
             <img
-              className="ps-5 hover:scale-150 transition ease-in-out delay-100 hover:rotate-180" src="/images/goal 2.png"
-              src="/images/add-friend 1.png"
+              className="ps-5 hover:scale-150 transition ease-in-out delay-100 hover:rotate-180"
+              src="https://i.ibb.co/z4wPLt4/add-friend.png"
               alt=""
-              srcset=""
             />
             <p className="text-white ps-5  font-bold text-base"> Step 2: </p>
 
             <p className="text-white   font-bold text-base">
-              Create or  Join<br /> <span className="ps-5">Group</span> 
+              Create or Join
+              <br /> <span className="ps-5">Group</span>
             </p>
           </div>
         </div>
@@ -42,46 +50,41 @@ function App() {
           className="absolute lg:bottom-[240px]  lg:left-[465px]
           bottom-[140px] lg:rotate-0 rotate-45  left-[475px]
           "
-          src="/images/Arrow 2.png"
+          src="https://i.ibb.co/D4b2sqY/Arrow-2.png"
           alt=""
         />
-           <div className="bg-blue-500 flex items-center justify-center lg:h-[160px] lg:w-[160px] h-[200px] w-[200px] rounded-full absolute lg:left-[650px] lg:bottom-[100px] left-[480px] -bottom-[130px]">
+        {/* this is 3rd circle */}
+        <div className="bg-blue-500 flex items-center justify-center lg:h-[160px] lg:w-[160px] h-[200px] w-[200px] rounded-full absolute lg:left-[650px] lg:bottom-[100px] left-[480px] -bottom-[130px]">
           <div>
             <img
-              className="ps-5 hover:scale-150 transition ease-in-out delay-100 hover:rotate-180" src="/images/goal 2.png"
-              src="/images/pay-per-click 1.png"
+              className="ps-5 hover:scale-150 transition ease-in-out delay-100 hover:rotate-180"
+              src="https://i.ibb.co/Lg2sj5t/pay-per-click-1.png"
               alt=""
-              srcset=""
             />
             <p className="text-white ps-5  font-bold text-base"> Step 3: </p>
 
             <p className="text-white   font-bold text-base">
-          
-Pay amount <br /> <span className="ps-3">on time</span>
+              Pay amount <br /> <span className="ps-3">on time</span>
             </p>
           </div>
         </div>
 
         <img
           className="absolute lg:bottom-[160px] lg:rotate-0 rotate-180  lg:left-[810px] -bottom-[130px]  left-[220px]"
-          src="/images/Arrow 3.png"
+          src="https://i.ibb.co/Wg1zLKK/Arrow-3.png"
           alt=""
         />
-         <div className="bg-blue-500 flex items-center justify-center lg:h-[160px] lg:w-[160px] h-[200px] w-[200px] rounded-full absolute left-[30px] -bottom-[300px] lg:left-[1040px] lg:bottom-[200px]">
+        {/* this is 4th circle */}
+        <div className="bg-blue-500 flex items-center justify-center lg:h-[160px] lg:w-[160px] h-[200px] w-[200px] rounded-full absolute left-[30px] -bottom-[300px] lg:left-[1040px] lg:bottom-[200px]">
           <div>
             <img
-              className="ps-5 hover:scale-150 transition-all ease-in-out delay-100  hover:-rotate-180" src="/images/goal 2.png"
-              src="/images/gift-card 1.png"
+              className="ps-5 hover:scale-150 transition-all ease-in-out delay-100  hover:-rotate-180"
+              src="https://i.ibb.co/RHDNxfP/gift-card-1.png"
               alt=""
-              srcset=""
             />
             <p className="text-white ps-5  font-bold text-base"> Step 4: </p>
 
-            <p className="text-white   font-bold text-base">
-          
-  
-Earn Rewards
-            </p>
+            <p className="text-white   font-bold text-base">Earn Rewards</p>
           </div>
         </div>
       </div>
